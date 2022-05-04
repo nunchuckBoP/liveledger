525de21df475a7ccc0b4e50ea993d0bca42d5e17
nunchuckbop <cj@neechsoft.com>
2022-05-03 19:23:04 -0500

    modified hosts

--- liveledger/settings.py
+++ liveledger/settings.py
@@ -28,11 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-                'localhost', 
-                'liveledger.panici.home',
-                'liveledger.neechsoft.com',
-                ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
